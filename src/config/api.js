7c1e51fc7d357545394ec667b2265618c7665447
FrongTH <rattapon.san@gmail.com>
2025-09-14 10:47:32 +0700

    ignore api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // API Configuration
 // Change this URL to update the API endpoint globally across the entire application
 
-const API_BASE_URL = 'http://192.168.1.144:8080/api/v1'
+const API_BASE_URL = 'http://192.168.1.181:8080/api/v1'
 
 export default API_BASE_URL
